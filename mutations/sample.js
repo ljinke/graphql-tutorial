6c3623c0b280bba52df9a07883da3f906cfa8aa7
Jackie Liu <jinke.liu@autodesk.com>
2019-07-17 18:25:59 +0800

    Add mutations

---
+++ mutations/sample.js
@@ -0,0 +1,12 @@
+{
+    mutation {
+      addStudent_returns_object(collegeId: "col-101", firstName: "Susan", lastName: "George") {
+        id
+        firstName
+        college {
+          id
+          name
+        }
+      }
+    }
+  }
--- resolvers.js
+++ resolvers.js
@@ -29,7 +29,30 @@ const Query = {
 const Student = {
   fullName: (root, args, context, info) => {
     return root.firstName + " " + root.lastName;
+  },
+  college: root => {
+    return db.colleges.get(root.collegeId);
   }
 };
 
-module.exports = { Query, Student };
+const Mutation = {
+  createStudent: (root, args, context, info) => {
+    return db.students.create({
+      collegeId: args.collegeId,
+      firstName: args.firstName,
+      lastName: args.lastName
+    });
+  },
+  // new resolver function
+  addStudent_returns_object: (root, args, context, info) => {
+    const id = db.students.create({
+      collegeId: args.collegeId,
+      firstName: args.firstName,
+      lastName: args.lastName
+    });
+
+    return db.students.get(id);
+  }
+};
+
+module.exports = { Query, Student, Mutation };
