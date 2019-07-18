f44d907ae7688909fce45c5bec428d09493ca8a2
Jackie Liu <jinke.liu@autodesk.com>
2019-07-18 17:30:07 +0800

    Update mutation request

--- src/client/mutations/sample.js
+++ src/client/mutations/sample.js
@@ -1,12 +1,11 @@
-{
-    mutation {
-      addStudent_returns_object(collegeId: "col-101", firstName: "Susan", lastName: "George") {
-        id
-        firstName
-        college {
-          id
-          name
-        }
-      }
+mutation {
+  addStudent_returns_object(collegeId: "col-101", firstName: "Susan", lastName: "George") {
+    id
+    firstName
+    college {
+      id
+      name
     }
   }
+}
+  
